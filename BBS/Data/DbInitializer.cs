c2876ff75df4598858954ee724b5561fd57b77bb
Lunatic <2972512952@qq.com>
2018-02-27 20:43:06 +0800

    2018/2/27 项目修改

--- BBS/Data/DbInitializer.cs
+++ BBS/Data/DbInitializer.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BBS.Data;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 
-namespace BBS.Data
+namespace BBS.Models
 {
     public class DbInitializer
     {
+        public static void Initialize(BBSContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            var users = new User[]
+            {
+                new User{Name="000000",Password="123456",AddTime=DateTime.Parse("2018-02-27"),State=State.Offline}
+            };
+            foreach(User u in users)
+            {
+                context.Users.Add(u);
+            }
+            context.SaveChanges();
+        }
     }
 }
